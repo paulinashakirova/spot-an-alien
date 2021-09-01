ec75e09188560b8a833a6923c18e1b26ef5cc942
PaulinaShakirova <pollisha.jeans@gmail.com>
2021-09-01 17:04:08 +0200

    initial commit + 1st part of the game done

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,64 @@
 import { createStore } from 'vuex'
 
 export default createStore({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
-})
+	state: {
+		uiState: 'start',
+		characterChoices: ['baker', 'mechanic', 'artist'],
+		character: '',
+		questionIndex: 0,
+		score: 0,
+		questions: [
+			{
+				question: `What's your dog's name?`,
+				baker: 'Woofgang Puck',
+				mechanic: 'Alf',
+				artist: 'Salvador Dogi'
+			},
+			{
+				question: `What's your favorite hobby?`,
+				baker: 'Extreme ironing',
+				mechanic: 'Bacon santa cosplay',
+				artist: 'Mimosas'
+			},
+			{
+				question: `What's your favorite color?`,
+				baker: 'turquoise',
+				mechanic: 'yellow',
+				artist: 'transparent'
+			},
+			{
+				question: `Is cereal soup?`,
+				baker: "You can't be serieal",
+				mechanic: 'Yes, I am Jason Lengstorf',
+				artist: 'wut'
+			},
+			{
+				question: `What’s the most imaginative insult you can come up with?`,
+				baker: "You're a substitute teacher with no lesson plan",
+				mechanic: 'Yer face is a melted welly',
+				artist: 'You eat buttons off the remote'
+			},
+			{
+				question: `If peanut butter wasn’t called peanut butter, what would it be called?`,
+				baker: 'legoome',
+				mechanic: 'brown paste',
+				artist: 'groundnut smoosh'
+			}
+		]
+	},
+	mutations: {
+		// we want to call these 2 mutations and it is called commiting
+		pickCharacter(state, character) {
+			state.character = character;
+		},
+		updateUiState(state, uiState) {
+			state.uiState = uiState;
+    }
+    // ,
+    // pickQuestion(state, questionIndex) {
+    //   state.questionIndex = questionIndex;
+    // }
+	},
+	actions: {},
+	modules: {}
+});
